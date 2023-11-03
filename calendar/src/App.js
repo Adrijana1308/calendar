7f10196590f9ceaed9249bc0616fe7e3fffbb56b
Adrijana1308 <adrijanababic4@gmail.com>
2023-11-03 11:52:28 +0100

    moment, controller and calendar

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import './App.css';
+import { CalendarController } from './CalendarController';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <CalendarController />
   );
 }
 
